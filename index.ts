1542af22162741d5c62f263095178e5cd36a4a58
Juan Campa <juancampa@gmail.com>
2023-06-09 15:39:34 -0400

    Add rename

--- index.ts
+++ index.ts
@@ -1,30 +1,50 @@
-import { state, root } from 'membrane';
+import { state as _state, root } from "membrane";
 
-state.nextKey = state.nextKey ?? 1;
+interface Item {
+  node: any;
+  tags: string[];
+}
+interface State {
+  items: { [key: string]: Item };
+}
+
+// Make state typed
+const state = _state as State;
+
+// Initialize state
 state.items = state.items ?? {};
 
 export const Root = {
-  put: ({ args: { node, name }}) => {
-    let key;
-    if (name != undefined) {
-      key = name;
-    } else {
-       do {
-         key = `node${state.nextKey++}`;
-       } while (state.items[key] !== undefined);
+  put: ({ args: { node, name } }) => {
+    let key: string = name;
+    let i = name ? 2 : 1;
+    while (!key || state.items[key] !== undefined) {
+      key = `${name ?? "node"}${i++}`;
     }
-    state.items[key] = node;
+    state.items[key] = { node, tags: [] };
     return key;
   },
-  remove: ({ args: { name }}) => {
+  remove: ({ args: { name } }) => {
     delete state.items[name];
   },
-  one: ({ args: { name }}) => state.items[name],
-  page: () => {
-    return {
-      items: Object.entries(state.items).map(([name, node]) => ({ gref: root.one({ name }), name, node })),
-      // TODO: paginate
-      next: null
+  one: ({ args: { name } }) => state.items[name],
+  rename: ({ args: { newName, oldName } }) => {
+    if (newName !== oldName) {
+      state.items[newName] = state.items[oldName];
+      delete state.items[oldName];
     }
-  }
-}
+  },
+  page: ({ args: { tag } }) => {
+    return {
+      items: Object.entries(state.items)
+        .filter((entry) => !tag || entry[1].tags.includes(tag))
+        .map(([name, { node, tags }]) => ({
+          gref: root.one({ name }),
+          name,
+          node,
+        })),
+      // TODO: paginate
+      next: null,
+    };
+  },
+};
